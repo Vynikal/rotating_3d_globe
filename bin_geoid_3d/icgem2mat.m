5641630856a70e15cc886e7b9be901011b277ed4
Jakub Vynikal <Vynikal@seznam.cz>
2020-08-02 12:06:22 +0200

    Support multiple epochs
    The script now adds a third dimension equal to time epoch. Indexes are mapped in M variable, which will be used in cti_icgem_model.

--- bin_geoid_3d/icgem2mat.m
+++ bin_geoid_3d/icgem2mat.m
@@ -84,11 +84,16 @@ for i=1:length(soub)
             x=str2num(s(5:end));
             if ~strcmp(s(1:4),'gfc ')
                 i_t0 = i_t0+1;
-                t(i_t0) = x(7);
+                y(i_t0) = x(7);
             end
             s=fgets(fid);
         end
-        t = unique(t); id = 1:length(t); M = containers.Map(t,id);
+        y = unique(y); id = 1:length(y); T = containers.Map(y,id);
+        % key for dates
+        [yr,mn,dy]=ymd2cal(y/1e4);
+        yrd=jd2yr(cal2jd(yr,mn,dy));
+        yrd=round(yrd*10000)/10000;
+        M = containers.Map(yrd,id);
         i_t0=0; fseek(fid,line1,'bof');
     end
       s=fgets(fid);
@@ -111,19 +116,19 @@ for i=1:length(soub)
             i_gfc=i_gfc+1;
          elseif strcmp(s(1:4),'gfct')
             if isempty(cnm_t0)
-               [status, result] = grep('-c','gfct ', filename);
+               [status, result] = grep('-c','gfct  ', filename);
                i1=str2double(result.result);
                if i1==0; error_ab('Problem with t0'); end
                cnm_t0=zeros(i1,3);
             end
-            i_t0=i_t0+1;
             if isempty(form)
                 time = x(end);
                 ti = 1;
             else
                 time = x(end-1);
-                ti = M(time);
+                ti = T(time);
             end
+            i_t0=i_t0+1;
             [yr,mn,dy]=ymd2cal(time/1e4);
             yrd=jd2yr(cal2jd(yr,mn,dy));
             cnm_t0(i_t0,:)=[n m yrd];
@@ -135,16 +140,16 @@ for i=1:length(soub)
             end
          elseif strcmp(s(1:4),'trnd') || strcmp(s(1:3),'dot') 
             if isempty(cnm_trnd)
-               [status, result] = grep('-c','trnd ', filename);
+               [status, result] = grep('-c','trnd  ', filename);
                i1=str2double(result.result);
-               if i1==0; [status, result] = grep('-c','dot ', filename); i1=str2double(result.result); end
+               if i1==0; [status, result] = grep('-c','dot  ', filename); i1=str2double(result.result); end
                if i1==0; error_ab('Problem with trnd'); end
                cnm_trnd=zeros(i1,3); snm_trnd=cnm_trnd;
             end
             i_trnd=i_trnd+1;
             if ~isempty(form)
                 t = x(end-1);
-                t_trnd(i_trnd) = M(t);
+                t_trnd(i_trnd) = T(t);
             end
             cnm_trnd(i_trnd,:)=[n m x(3)];
             snm_trnd(i_trnd,:)=[n m x(4)];
@@ -158,7 +163,7 @@ for i=1:length(soub)
             i_acos=i_acos+1;
             if ~isempty(form)
                 t = x(end-2);
-                t_acos(i_acos) = M(t);
+                t_acos(i_acos) = T(t);
             end
             cnm_acos(i_acos,:)=[n m x(3) x(end)];
             snm_acos(i_acos,:)=[n m x(4) x(end)];
@@ -172,7 +177,7 @@ for i=1:length(soub)
             i_asin=i_asin+1;
             if ~isempty(form)
                 t = x(end-2);
-                t_asin(i_asin) = M(t);
+                t_asin(i_asin) = T(t);
             end
             cnm_asin(i_asin,:)=[n m x(3) x(end)];
             snm_asin(i_asin,:)=[n m x(4) x(end)];
@@ -183,33 +188,61 @@ for i=1:length(soub)
       end
       fclose(fid);
       
-      % vymenit radky za dalsi dimenzi
-%       if ~isempty(form)
-%         for i = 1:length(t_trnd)
-%             for j = 1:max(id)
-%             cnm_tr(:,:,t_trnd(i)) = cnm_trnd(i,:);
-%             end
-%         end
-%         for i = 1:length(t_acos)
-%             for j = 1:max(id)
-%             cnm_ac(:,:,t_acos(i)) = cnm_acos(i,:);
-%             end
-%         end
-%         for i = 1:length(t_asin)
-%             for j = 1:max(id)
-%             cnm_as(:,:,t_asin(i)) = cnm_asin(i,:);
-%             end
-%         end
-%       end
-      
+      if ~isempty(form)
+        cnm_tr = zeros(length(unique(cnm_trnd(:,1)*100+cnm_trnd(:,2))),3,max(id)); snm_tr = cnm_tr;
+        cnm_ac = zeros(length(unique(cnm_acos(:,1)*100+cnm_acos(:,2)+cnm_acos(:,4)))-2,4,max(id)); cnm_as = cnm_ac; snm_ac = cnm_ac; snm_as = cnm_ac;
+        j = 1;
+        cnm_tr(j,:,t_trnd(1)) = cnm_trnd(1,:);
+        snm_tr(j,:,t_trnd(1)) = snm_trnd(1,:);
+        for i = 2:length(t_trnd)
+            if cnm_trnd(i-1,1) ~= cnm_trnd(i,1) || cnm_trnd(i-1,2) ~= cnm_trnd(i,2)
+                j = j+1;
+            end
+            cnm_tr(j,:,t_trnd(i)) = cnm_trnd(i,:);
+            snm_tr(j,:,t_trnd(i)) = snm_trnd(i,:);
+        end
+        j = 1;
+        cnm_ac(j,:,t_acos(1)) = cnm_acos(1,:);
+        cnm_ac(j+1,:,t_acos(1)) = cnm_acos(2,:);
+        snm_ac(j,:,t_acos(1)) = snm_acos(1,:);
+        snm_ac(j+1,:,t_acos(1)) = snm_acos(2,:);
+        for i = 3:length(t_acos)
+            if cnm_acos(i-1,1) ~= cnm_acos(i,1) || cnm_acos(i-1,2) ~= cnm_acos(i,2)
+                j = j+2;
+            end
+            cnm_ac(j,:,t_acos(i)) = cnm_acos(i-1,:);
+            cnm_ac(j+1,:,t_acos(i)) = cnm_acos(i,:);
+            snm_ac(j,:,t_acos(i)) = snm_acos(i-1,:);
+            snm_ac(j+1,:,t_acos(i)) = snm_acos(i,:);
+        end
+        j = 1;
+        cnm_as(j,:,t_asin(1)) = cnm_asin(1,:);
+        cnm_as(j+1,:,t_asin(1)) = cnm_asin(2,:);
+        snm_as(j,:,t_asin(1)) = snm_asin(1,:);
+        snm_as(j+1,:,t_asin(1)) = snm_asin(2,:);
+        for i = 3:length(t_asin)
+            if cnm_asin(i-1,1) ~= cnm_asin(i,1) || cnm_asin(i-1,2) ~= cnm_asin(i,2)
+                j = j+2;
+            end
+            cnm_as(j,:,t_asin(i)) = cnm_asin(i-1,:);
+            cnm_as(j+1,:,t_asin(i)) = cnm_asin(i,:);
+            snm_as(j,:,t_asin(i)) = snm_asin(i-1,:);
+            snm_as(j+1,:,t_asin(i)) = snm_asin(i,:);
+        end
+        cnm_trnd = cnm_tr; cnm_acos = cnm_ac; cnm_asin = cnm_as; snm_trnd = snm_tr; snm_acos = snm_ac; snm_asin = snm_as;
+      else
+          yrd = unique(cnm_t0(:,3));
+          M = containers.Map(yrd, 1:length(yrd));
+      end
+
       modelname=header.modelname;
 
       %it is possible to limit the maximum degree read from the gfc file 
       n_gfc=i_gfc; n_t0=i_t0; n_trnd=i_trnd; n_acos=i_acos; n_asin=i_asin; 
-      if n_t0; cnm_t0=cnm_t0(1:n_t0,:,:); end
-      if n_trnd; cnm_trnd=cnm_trnd(1:n_trnd,:,:); snm_trnd=snm_trnd(1:n_trnd,:); end
-      if n_acos; cnm_acos=cnm_acos(1:n_acos,:,:); snm_acos=snm_acos(1:n_acos,:); end
-      if n_asin; cnm_asin=cnm_asin(1:n_asin,:,:); snm_asin=snm_asin(1:n_asin,:); end
+%       if n_t0; cnm_t0=cnm_t0(1:n_t0,:,:); end
+%       if n_trnd; cnm_trnd=cnm_trnd(1:n_trnd,:,:); snm_trnd=snm_trnd(1:n_trnd,:); end
+%       if n_acos; cnm_acos=cnm_acos(1:n_acos,:,:); snm_acos=snm_acos(1:n_acos,:); end
+%       if n_asin; cnm_asin=cnm_asin(1:n_asin,:,:); snm_asin=snm_asin(1:n_asin,:); end
       if n_t0~=n_trnd || n_acos~=n_asin
          error_ab('Problem with numbers of TVG terms.');
       end
@@ -217,7 +250,7 @@ for i=1:length(soub)
 
       if ~exist(adr_kam,'file'), mkdir(adr_kam); end
       if ~exist([adr_kam 'gfc'],'file'), mkdir([adr_kam 'gfc']); end
-      eval(sprintf('save %s%s.mat cnm snm ecnm esnm header modelname n_t0 n_trnd n_acos n_asin cnm_t0 cnm_trnd snm_trnd cnm_acos snm_acos cnm_asin snm_asin;',adr_kam,soub1));
+      eval(sprintf('save %s%s.mat cnm snm ecnm esnm header modelname n_t0 n_trnd n_acos n_asin cnm_t0 cnm_trnd snm_trnd cnm_acos snm_acos cnm_asin snm_asin M;',adr_kam,soub1));
       copyfile([adr_data soub1 '.gfc'],[adr_kam 'gfc']);
       fprintf('  Resulting file %s.mat was moved into folder: %s\n',soub1,adr_kam);
       fprintf('  Original file  %s.gfc was moved into folder: %sgfc\n',soub1,adr_kam);
